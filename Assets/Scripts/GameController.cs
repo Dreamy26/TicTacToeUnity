134d94f3877dbdcbfd43da19225d2b5bef226545
AWilson <dreamy26@icloud.com>
2021-02-13 13:47:39 -0500

    Adding whosturn, on click to identify marked space

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,6 +11,7 @@ public class GameController : MonoBehaviour
     public GameObject[] turnIcon; // displays who turn it is
     public Sprite[] playerIcons; // 0 = x icon and 1 = o icon
     public Button[] tictactoeSpaces; // playable space for our game
+    public int[] markSpaces;  // ID's which space was marked by which player
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +30,10 @@ public class GameController : MonoBehaviour
           tictactoeSpaces[i].interactable = true;
           tictactoeSpaces[i].GetComponent<Image>().sprite = null;  
         }
+        for (int i = 0; i < markedSpaces.Length; i++)
+        {
+            markedSpaces[i] = -1; // change the value of the corresponding element inside this array to the value of whoseTurn it is
+        }
     }
 
     // Update is called once per frame
@@ -43,6 +48,8 @@ public class GameController : MonoBehaviour
         tictactoeSpaces[WhichNumber].image.sprite = playerIcons[whoseTurn];
         tictactoeSpaces[WhichNumber].interactable = false;
 
+        markedSpaces[WhichNumber] = whoseTurn; // the method used to identify which space has been marked by which player. 
+
         // Change who's turn it is 
         if (whoseTurn == 0)
         {
@@ -55,6 +62,8 @@ public class GameController : MonoBehaviour
             whoseTurn = 0;
             turnIcon[0].SetActive(true);
             turnIcon[1].SetActive(false);
+
+            
         }
     }
 }
